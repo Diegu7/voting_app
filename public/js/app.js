ba2f2c5bc63e1d4beef0e2025be43fbc9870423f
Diegu Montes <mecayounbloque@gmail.com>
2018-03-11 23:11:51 -0600

    created product

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,36 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className='ui unstackable items'>
-        Hello, friend! I am a basic React component.
+        <Product />
+      </div>
+    );
+  }
+}
+
+class Product extends React.Component {
+  handleUpVote = () => {
+    this.props.onVote(this.props.id);
+  }
+
+  render() {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src="images/products/image-aqua.png" />
+        </div>
+        <div className='middle aligned content'>
+          <div className='description'>
+            <a>Fort Knight</a>
+            <p>Authentic Renaissance actors delivered etc</p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src="images/avatars/daniel.jpg"
+            />
+          </div>
+        </div>
       </div>
     );
   }
